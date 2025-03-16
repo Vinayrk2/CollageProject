f699c076eb3d1f166f24c0f64a3b897cb97b1f44
Vinayrk2 <v277772@gmail.com>
2025-03-16 08:52:38 -0700

    Add

--- kruskal_project/kruskal_project/settings.py
+++ kruskal_project/kruskal_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +127,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-import os
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'kruskal_app/static')]
 
